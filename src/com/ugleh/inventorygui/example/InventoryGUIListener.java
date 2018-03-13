45c60e459f904522a0e8c018bf1fdc14da089f57
Ugleh <troti@ymail.com>
2018-03-13 06:41:28 -0400

    Added InventoryButtonBuilder example

--- src/com/ugleh/inventorygui/example/InventoryGUIListener.java
+++ src/com/ugleh/inventorygui/example/InventoryGUIListener.java
@@ -11,74 +11,96 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 import com.ugleh.inventorygui.InventoryButton;
+import com.ugleh.inventorygui.InventoryButtonBuilder;
 import com.ugleh.inventorygui.InventoryGUI;
 import com.ugleh.inventorygui.InventoryMenu;
-import com.ugleh.inventorygui.events.ButtonClickedEvent;
+import com.ugleh.inventorygui.events.ButtonClickEvent;
 import com.ugleh.inventorygui.events.MenuOpenedEvent;
 
 public class InventoryGUIListener implements Listener {
 	public HashMap<String, InventoryMenu> ourPersonalMenus = new HashMap<String, InventoryMenu>();
-	
-	public InventoryGUIListener(Plugin plugin)
-	{
-		InventoryGUI invGUI = new InventoryGUI(plugin);
-		
-		InventoryMenu invMenu = new InventoryMenu(ChatColor.DARK_PURPLE + "Example Menu", 9);
 
-		InventoryButton sendHome = new InventoryButton(ChatColor.GOLD + "Go to Spawn", new ItemStack(Material.BED), "cp_spawn");
-		sendHome.setLoreWordWrapped("Press to send yourself back to spawn.");
-		InventoryButton killSelf = new InventoryButton(ChatColor.GOLD + "Kill Self", new ItemStack(Material.ARROW), "cp_kill");
+	public InventoryGUIListener(Plugin plugin) {
+		InventoryGUI inventoryGUI = new InventoryGUI(plugin);
+		InventoryMenu invMenu = new InventoryMenu(ChatColor.DARK_PURPLE + "Example Menu", 9);
+		inventoryGUI.registerMenu(invMenu);
+
+		InventoryButton sendToSpawn = new InventoryButtonBuilder()
+				// You can create an item using the InventoryButtonBuilder,
+				// which lets you build the button with seperate methods.
+				.setDisplayName(ChatColor.GOLD + "Go to Spawn")
+				// Setting the Buttons Display Name
+				.setItemStack(new ItemStack(Material.BED))
+				// Setting the Button to the Material Type Bed, using ItemStack
+				// allows support for different data types.
+				.setFlag("cp_spawn")
+				// Setting a custom flag that will be accessed in the
+				// ButtonClickEvent
+				.setLoreWordWrapped("Press to send yourself back to spawn.")
+				// Setting the lore using the custom method LoreWordWrapped does
+				// the word wrapping for us :)
+				.build();
+		// Don't forget to build it after or else you wont get an
+		// InventoryButton.
+
+		InventoryButton killSelf = new InventoryButton(ChatColor.GOLD + "Kill Self", new ItemStack(Material.ARROW),
+				"cp_kill");
 		killSelf.setLoreWordWrapped("Press to kill your self. No really, just press.");
-		InventoryButton setFire = new InventoryButton(ChatColor.GOLD + "Set On Fire", new ItemStack(Material.LAVA_BUCKET), new ItemStack(Material.WATER_BUCKET), "fire");
-		setFire.setLoreWordWrapped("Toggle yourself on/off fire.");
-		InventoryButton reload = new InventoryButton(ChatColor.GOLD + "Reload", new ItemStack(Material.HOPPER), "c_reload");
+		killSelf.setPermission("bukkit.command.kill", ChatColor.RED + "You do not have permission to use that.", true);
+
+		InventoryButton setFire = new InventoryButtonBuilder()
+				// Set Display Name
+				.setDisplayName(ChatColor.GOLD + "Set On Fire")
+				// Set the button material.
+				.setItemStack(new ItemStack(Material.LAVA_BUCKET))
+				// This button will be toggable, so I have set its seperate
+				// button to a water bucket.
+				.setToggledItemStack(new ItemStack(Material.WATER_BUCKET))
+				// The coding for this will be too complex for just a single
+				// command to toggle fire/no fire, so I am just going give it a
+				// normal flag
+				.setFlag("fire")
+				// Setting its lore.
+				.setLoreWordWrapped("Toggle yourself on/off fire.")
+				// Build it
+				.build();
+
+		InventoryButton reload = new InventoryButton(ChatColor.GOLD + "Reload", new ItemStack(Material.HOPPER),
+				"c_reload");
 		reload.setLoreWordWrapped("Reload the server whenever with this button.");
-		
-		invMenu.addButton(0, sendHome);
+
+		invMenu.addButton(0, sendToSpawn);
 		invMenu.addButton(1, killSelf);
 		invMenu.addButton(2, setFire);
 		invMenu.addButton(8, reload);
-		
-		ourPersonalMenus.put("example_menu", invMenu);
-		invGUI.registerMenu(invMenu);
-		
 	}
+
 	@EventHandler
-	public void onButtonClickedEvent(ButtonClickedEvent e)
-	{
+	public void onButtonClickEvent(ButtonClickEvent e) {
 		InventoryButton b = e.getInventoryButton();
 		String flag = b.getFlag();
-		if(ourPersonalMenus.containsValue(e.getInventoryMenu()))
-		{
-			//This is a menu created by our plugin and not another using the same InventoryGUI.
-			if(flag.contains("cp_"))
-			{
-				//cp_ is a personal prefix that tells me it is a button which requires command execution when pressed by Player.
-				String command = flag.replace("cp_", "");
-				e.getPlayer().performCommand(command);
-			}else if(flag.contains("c_"))
-			{
-				//c_ is a personal prefix that tells me it is a button which requires command execution when pressed by Console.
-				String command = flag.replace("c_", "");
-				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
-			}else if(flag.equals("fire"))
-			{
-				if(!b.isToggleState())
-				{
-					b.setDisplayName(ChatColor.GOLD + "Remove Fire");
-					e.getPlayer().setFireTicks(1000);
-				}else
-				{
-					b.setDisplayName(ChatColor.GOLD + "Set on Fire");
-					e.getPlayer().setFireTicks(0);
-				}
+		// This is a menu created by our plugin and not another using the same InventoryGUI.
+		if (flag.contains("cp_")) {
+			// cp_ is a personal prefix that tells me it is a button which requires command execution when pressed by Player.
+			String command = flag.replace("cp_", "");
+			e.getPlayer().performCommand(command);
+		} else if (flag.contains("c_")) {
+			// c_ is a personal prefix that tells me it is a button which requires command execution when pressed by Console.
+			String command = flag.replace("c_", "");
+			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
+		} else if (flag.equals("fire")) {
+			if (!b.isToggleState()) {
+				b.setDisplayName(ChatColor.GOLD + "Remove Fire");
+				e.getPlayer().setFireTicks(1000);
+			} else {
+				b.setDisplayName(ChatColor.GOLD + "Set on Fire");
+				e.getPlayer().setFireTicks(0);
 			}
 		}
 	}
-	
+
 	@EventHandler
-	public void onMenuOpenedEvent(MenuOpenedEvent e)
-	{
-		
+	public void onMenuOpenedEvent(MenuOpenedEvent e) {
+
 	}
 }

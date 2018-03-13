f882a4ba272f45e06d46e9c79fe3f1308ebd26c1
Ugleh <troti@ymail.com>
2018-03-13 07:20:52 -0400

    Added onDisable to prevent menus open after reload

--- src/com/ugleh/inventorygui/example/ExampleInventoryGUI.java
+++ src/com/ugleh/inventorygui/example/ExampleInventoryGUI.java
@@ -1,7 +1,13 @@
 package com.ugleh.inventorygui.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.ugleh.inventorygui.InventoryMenu;
 
 public class ExampleInventoryGUI extends JavaPlugin{
 
@@ -16,6 +22,20 @@ public class ExampleInventoryGUI extends JavaPlugin{
 		Bukkit.getPluginManager().registerEvents(listener = new InventoryGUIListener(this), this);
 	}
 	
+	
+	@Override
+	public void onDisable()
+	{
+		for(Entry<String, InventoryMenu> entry : this.getListener().ourPersonalMenus.entrySet())
+		{
+			List<HumanEntity> clonedViewers = new ArrayList<HumanEntity>(entry.getValue().getInventory().getViewers());
+			
+			for(HumanEntity player : clonedViewers)
+			{
+				player.closeInventory();
+			}
+		}
+	}
 	
 	public static ExampleInventoryGUI getInstance()
 	{
